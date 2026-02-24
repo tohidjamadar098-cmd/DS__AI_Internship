ea8cea42b31fbea12f8816cea57504f0e8e1151e
mahammadtouhid yusufkhan jamadar <tohidjamadar098@gmail.com>
2026-02-24 15:23:27 +0530

    initial commit

---
+++ src/mini_project/mini_project1_eda.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "08261b78-7107-4b49-b3d3-3c3bf80dbda3",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
